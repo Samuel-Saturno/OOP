81e2fe831a747269e92d5d404de860863174c755
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-26 08:49:10 -0300

    Create Data.java

---
+++ Exemplos/Ex1/Data.java
@@ -0,0 +1,24 @@
+package Exemplos.Ex1;
+
+public class Data {
+    int dia, ano, mes;
+
+    //ele n vai receber nenhum argumento pq os argumentos (data, mes e ano) ja estao na classe
+    boolean dataEhValida(){
+        return (dia>=1 && dia<=31 && mes>=1 && mes<=12 && ano>=0);
+    }
+    
+    void inicializaData(int d, int m, int a){
+        dia = d;
+        mes = m;
+        ano = a;;
+    }
+
+    void mostraData(){
+        //System.out.println("Dia: "+dia);
+        //System.out.println("Mes: "+mes);
+        //System.out.println("Ano: "+ano);
+        System.out.println(dia + "/" + mes + "/" + ano);
+    }
+
+}
