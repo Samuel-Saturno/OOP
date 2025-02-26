849b3f1fd622cbc590bde3212c90503e67eebd72
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-26 08:48:10 -0300

    Create Main.java

---
+++ Exemplos/ClasseExecutavel/Main.java
@@ -0,0 +1,10 @@
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Boas vindas");
+    }
+}
+
+// Para executar usar os comandos
+// javac NomeArquivo.Extensao
+// java NomeArquivo
