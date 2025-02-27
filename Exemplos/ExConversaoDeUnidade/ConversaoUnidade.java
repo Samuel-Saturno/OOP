bee5a0263a966260aa8ab6543a374259d52d3920
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-27 16:53:54 -0300

    Create ConversaoUnidade.java

---
+++ Exemplos/ExConversaoDeUnidade/ConversaoUnidade.java
@@ -0,0 +1,15 @@
+package Exemplos.ExConversaoDeUnidade;
+
+public class ConversaoUnidade {
+    public static float polegadasParaCentimetros(float polegadas){
+        return polegadas*2.54F;
+    }
+
+    public static float pesParaCentimetros(float pes){
+        return pes*30.48F;
+    }
+
+    public static float milhasParaCentimetros(float milhas){
+        return milhas*1.609F;
+    }
+}
