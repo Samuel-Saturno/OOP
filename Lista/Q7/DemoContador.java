fdb0687e3ae964187088cfcacd4f1f6b8c1e270f
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-27 11:21:47 -0300

    Create DemoContador.java

---
+++ Lista/Q7/DemoContador.java
@@ -0,0 +1,8 @@
+package Lista.CriaçãoDeClassesP2.Q7;
+
+public class DemoContador {
+    public static void main(String[] args) {
+        Contador cont = new Contador();
+        cont.menu();
+    }
+}
