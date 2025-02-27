111b57cf46431f97cce4b638f601c49622e3210a
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-27 15:55:00 -0300

    Create DemoLampada.java

---
+++ Lista/Q8/DemoLampada.java
@@ -0,0 +1,10 @@
+// OBS: Para que o codigo execute é necessario alterações no cod da questão 7
+
+package Lista.CriaçãoDeClassesP2.Q8;
+
+public class DemoLampada {
+    public static void main(String[] args) {
+        Lampada lamp = new Lampada();
+        lamp.menu();
+    }
+}
