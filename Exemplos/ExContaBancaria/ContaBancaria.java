8f31e5eb52e8a1323c078057897a2d47eb9a6c08
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-27 08:01:03 -0300

    Create ContaBancaria.java

---
+++ Exemplos/ExContaBancaria/ContaBancaria.java
@@ -0,0 +1,27 @@
+package Exemplos.ExContaBancaria;
+
+public class ContaBancaria {
+    private String nome;
+    private float saldo;
+    private boolean especial;
+
+    public ContaBancaria(String nome, float saldo, boolean especial) {
+        this.nome = nome;
+        this.saldo = saldo;
+        this.especial = especial;
+    }
+
+    public ContaBancaria(String nome) {
+        this.nome = nome;
+        this.saldo = 0.0F;
+        this.especial = false;
+    }
+
+    public String toString(){
+        String res = "";
+        res += "Correntista: " + nome + "\n";
+        res += "Saldo: " + saldo + "\n";
+        res += (especial ? "Conta especial" : "Conta comum");
+        return res;
+    }
+}
