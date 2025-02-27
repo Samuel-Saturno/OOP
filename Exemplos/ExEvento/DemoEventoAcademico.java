f130adaa3f38d767accd92a80c38e33f0b135338
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-27 08:05:07 -0300

    Create DemoEventoAcademico.java

---
+++ Exemplos/ExEvento/DemoEventoAcademico.java
@@ -0,0 +1,8 @@
+package Exemplos.ExEvento;
+
+public class DemoEventoAcademico {
+    public static void main(String[] args) {
+        EventoAcademico ev = new EventoAcademico("ECOP", "UFERSA", 500);
+        ev.mostraEvento();
+    }
+}
