f740e89df09f99081d59b8fdab78ffc1ad2374e2
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-27 16:37:44 -0300

    Update Data.java

--- Exemplos/ExData/Data.java
+++ Exemplos/ExData/Data.java
@@ -1,24 +1,58 @@
-package Exemplos.ExData;
+package Exemplos.ExDatra;
 
 public class Data {
-    int dia, ano, mes;
+    private int dia, ano, mes;
+
+    
+
+    public Data(int dia, int ano, int mes) {
+        this.dia = dia;
+        this.ano = ano;
+        this.mes = mes;
+
+        if (!dataEhValida()) {
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2000;
+        }
+    }
 
     //ele n vai receber nenhum argumento pq os argumentos (data, mes e ano) ja estao na classe
     boolean dataEhValida(){
         return (dia>=1 && dia<=31 && mes>=1 && mes<=12 && ano>=0);
     }
     
-    void inicializaData(int d, int m, int a){
+    public void inicializaData(int d, int m, int a){
         dia = d;
         mes = m;
-        ano = a;;
+        ano = a;
+        if (!dataEhValida()) {
+            dia = 1;
+            mes = 1;
+            ano = 2000;
+        }
     }
 
-    void mostraData(){
-        //System.out.println("Dia: "+dia);
-        //System.out.println("Mes: "+mes);
-        //System.out.println("Ano: "+ano);
+    public void mostraData(){
         System.out.println(dia + "/" + mes + "/" + ano);
+    }
+
+    static Data criaNatal(int ano){
+        if (ano > 0) {
+            return new Data(25, 12, ano);
+        } else{
+            System.out.println("Ano invalido!");
+            return null;
+        }
+    }
+
+    static Data criaData(int dia, int mes, int ano){
+        if (dia>=1 && dia<=31 && mes>=1 && mes<=12 && ano>=0) {
+            return new Data(dia , mes, ano);
+        } else{
+            System.out.println("Ano invalido!");
+            return null;
+        }
     }
 
 }
