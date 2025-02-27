b60bd4e8bd1a5db4f4aac9b1e7fde132df013d94
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-02-27 10:14:36 -0300

    Create DemoElevador.java

---
+++ Lista/Q6/DemoElevador.java
@@ -0,0 +1,8 @@
+package Lista.CriaçãoDeClassesP2.Q6;
+
+public class DemoElevador {
+    public static void main(String[] args) {
+        Elevador e1 = new Elevador(6, 10);
+        e1.menu();
+    }
+}
