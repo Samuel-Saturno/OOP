dbdb312ab71029f53b3c1b00d41c8dd1b5b500c4
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-11 17:34:17 -0300

    Create DemoAgenda.java

---
+++ Lista/ArraysIhArrayLists/Q17/DemoAgenda.java
@@ -0,0 +1,10 @@
+package Lista.ArraysArrayLists.Q17;
+
+public class DemoAgenda {
+    public static void main(String[] args) {
+        Agenda comp = new Agenda();
+        comp.novoCompromisso(13, 11, 3, 2025);
+        comp.novoCompromisso(13, 12, 3, 2025);
+        comp.listaDia();
+    }
+}
