12f6b8e29fc614001d7e1ba0625dd76ff5dd10ca
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-11 16:53:41 -0300

    Create DemoDamas.java

---
+++ Exemplos/SegUnid/ExJogoDamas/DemoDamas.java
@@ -0,0 +1,8 @@
+package Exemplos.SegUnid.ExJogoDeDamas;
+
+public class DemoDamas {
+    public static void main(String[] args) {
+        Damas dama = new Damas();
+        System.out.println(dama);
+    }
+}
