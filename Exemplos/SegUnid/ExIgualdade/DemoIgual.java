4a91aa59af1338b25301d8a5181bed22970b65ac
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-12 09:50:44 -0300

    Create DemoIgual.java

---
+++ Exemplos/SegUnid/ExIgualdade/DemoIgual.java
@@ -0,0 +1,23 @@
+package Exemplos.SegUnid.ExIgualdade;
+
+public class DemoIgual {
+    public static void main(String[] args) {
+        if (Igual.ehIgual(2, 2)){
+            System.out.println("Inteiros iguais");
+        } else {
+            System.out.println("Inteiros diferentes");
+        }
+        
+        if (Igual.ehIgual(2.3, 2.3)){
+            System.out.println("Doubles iguais");
+        } else {
+            System.out.println("Dloubles diferentes");
+        }
+
+        if (Igual.ehIgual("UFERSA", "UERN")){
+            System.out.println("Strings iguais");
+        } else {
+            System.out.println("Strings diferentes");
+        }
+    }
+}
