1ad5b67359e87af28e84ec8f9c41cbc52e42ee2e
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-12 12:59:56 -0300

    Create DemoMatriz.java

---
+++ Lista/ArraysIhArrayLists/Q18/DemoMatriz.java
@@ -0,0 +1,9 @@
+package Lista.ArraysArrayLists.Q18;
+
+public class DemoMatriz {
+    public static void main(String[] args) {
+        Matriz mat = new Matriz();
+        mat.impressora();
+        mat.calculaDeterminante();
+    }
+}
