559037e5de9ea457637640ee5264046f02aaabc5
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-13 14:36:05 -0300

    Create Funcionario.java

---
+++ Exemplos/SegUnid/ReusoDeClasses/ExEmpresa/Funcionario.java
@@ -0,0 +1,19 @@
+package Exemplos.SegUnid.Reuso.ExEmpresa;
+
+public class Funcionario extends Pessoa {
+    private float salario;
+
+    public Funcionario(String nome, String identidade, float salario) {
+        super(nome, identidade);
+        this.salario = salario;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Salario= " + salario;
+    }
+}
