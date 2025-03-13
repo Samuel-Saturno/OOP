7d2b21163033325b573ff8e6836e2adbd51e549b
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-13 17:23:38 -0300

    Create Prefeito.java

---
+++ Lista/ReusoDeClasses/Q31/Prefeito.java
@@ -0,0 +1,15 @@
+package Lista.ReusoDeClasses.Q31;
+
+public class Prefeito extends Politico {
+    private String cidade;
+
+    public Prefeito(String nome, int idade, String partido, String cidade) {
+        super(nome, idade, partido);
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "; Municipio: " + cidade;
+    }
+}
