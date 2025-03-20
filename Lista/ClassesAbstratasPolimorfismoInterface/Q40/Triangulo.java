7bec8ae8b4d3a1f4dc7df64839c358a7d7e1bdbb
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-20 09:14:19 -0300

    Create Triangulo.java

---
+++ Lista/ClassesAbstratasPolimorfismoInterface/Q40/Triangulo.java
@@ -0,0 +1,8 @@
+package Lista.ClassesAbstratasPolimorfismoInterface.Q40;
+
+class Triangulo extends FiguraGeometrica {
+    @Override 
+    public void descricao(){
+        System.out.println("sou um Triangulo");
+    }
+}
