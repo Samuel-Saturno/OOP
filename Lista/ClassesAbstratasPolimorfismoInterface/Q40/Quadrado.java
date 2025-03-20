7b4a12f44eeba2eea38ba98a41d45e08c831795b
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-20 09:14:00 -0300

    Create Quadrado.java

---
+++ Lista/ClassesAbstratasPolimorfismoInterface/Q40/Quadrado.java
@@ -0,0 +1,8 @@
+package Lista.ClassesAbstratasPolimorfismoInterface.Q40;
+
+class Quadrado extends FiguraGeometrica {
+    @Override 
+    public void descricao(){
+        System.out.println("sou um Quadrado");
+    }
+}
