0cc5c8af223856563c22bd9f9e3427c10d2c56d4
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-03-20 09:11:23 -0300

    Create Circulo.java

---
+++ Lista/ClassesAbstratasPolimorfismoInterface/Q40/Circulo.java
@@ -0,0 +1,8 @@
+package Lista.ClassesAbstratasPolimorfismo_nterface.Q40;
+
+class Circulo extends FiguraGeometrica {
+    @Override 
+    public void descricao(){
+        System.out.println("sou um circulo");
+    }
+}
